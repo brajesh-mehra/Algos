57e4b6ad2538391e110b96f61becf0a52e96d726
Brajesh Mehra <brajesh.mehra@microfocus.com>
2022-07-17 13:08:48 +0530

    Level order traversal

--- BinaryTree/src/BinaryTree.java
+++ BinaryTree/src/BinaryTree.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTree {
 
     private static TreeNode root;
@@ -10,6 +13,33 @@ public class BinaryTree {
         TreeNode third = new TreeNode(3);
         TreeNode fourth = new TreeNode(4);
         TreeNode fifth = new TreeNode(10);
+
+        root = first;
+        first.left = second;
+        first.right = third;
+
+        second.left = fourth;
+
+        third.left = fifth;
+        return root;
+    }
+
+    // create binary tree
+    public TreeNode createBinaryTree2()
+    {
+        TreeNode first = new TreeNode(2);
+        TreeNode second = new TreeNode(4);
+        TreeNode third = new TreeNode(6);
+        TreeNode fourth = new TreeNode(3);
+        TreeNode fifth = new TreeNode(2);
+        TreeNode six = new TreeNode(4);
+
+        TreeNode seven = new TreeNode(1);
+        TreeNode eight = new TreeNode(5);
+        TreeNode nine = new TreeNode(6);
+        TreeNode ten = new TreeNode(9);
+        TreeNode eleven = new TreeNode(8);
+
 
         root = first;
         first.left = second;
@@ -46,5 +76,27 @@ public class BinaryTree {
         preOrder(root.left);
         preOrder(root.right);
         System.out.println(root.data + " ");
+    }
+
+    // Level Order Traversal
+    public void levelOrderTraversal(TreeNode root){
+        if(root == null) return;
+
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+
+        while (!q.isEmpty())
+        {
+            TreeNode temp = q.poll();
+            System.out.println(temp.data + " ");
+            if(temp.left != null)
+            {
+                q.offer(temp.left);
+            }
+            if (temp.right != null)
+            {
+                q.offer(temp.right);
+            }
+        }
     }
 }
--- BinaryTree/src/Main.java
+++ BinaryTree/src/Main.java
@@ -13,5 +13,8 @@ public class Main {
 
         System.out.println("Post order");
         bt.postOrder(bt.createBinaryTree());
+
+        System.out.println("Level order traversal");
+        bt.levelOrderTraversal(bt.createBinaryTree());
     }
 }
